21c85b779d9d9a97db3adc613c0063505d368480
Avinash Kokare <avinash.kokare@fitcircle.in>
2023-06-08 00:25:25 +0530

    Removed unwanted code

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,22 +1,10 @@
 "use client"
 import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import Link from 'next/link';
+
 
 const ContactUs = () => {
     const form = useRef();
     const [toggle, setToggle] = useState(false);
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('service_pt8oqu9', 'template_8mlc6i3', form.current, 'n6wpm0C1V5Zdjslse')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
 
     console.log("Hello i am toggle value : ", toggle);
     return (
